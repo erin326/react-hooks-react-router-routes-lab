0be188bb4d36c3e6282b440714848a83a928543d
erin <erinverkoulen93@gmail.com>
2021-07-26 15:05:16 -0500

    commit tree 80ccd89ef29130ce0faf75c9a6a6ea5f98d012fb

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,28 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  // <ul>{movie.genre.map(item => (
+  //   <li>{item.genre}</li>
+  // ))}</ul>
+  const genres =   movies.map(movie => (
+    <div key={movie.title}>Movie Title: {movie.title} | Movie Time: {movie.time} 
+      <ul>
+        {movie.genres.map((genre, index) => (
+          <li key={index}>{genre}</li>
+        ))}
+      </ul>
+  
+    </div>
+   
+  ))
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+    {genres}
+    </div>
+  )
 }
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,62 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const linkStyles = {
+  width: '160px',
+  padding:'14px',
+  margin: '0 6px 6 px',
+  background: 'orange',
+  textDecoration: 'none',
+  color: 'white'
+
+}
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return ( 
+    <div className='navbar'>
+      <NavLink
+      to='/'
+      exact
+      style={linkStyles}
+      activeStyle={{
+        background: 'pink'
+      }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+      to='/movies'
+      exact
+      style={linkStyles}
+      activeStyle={{
+        background: 'pink'
+      }}
+      >
+        Movies
+      </NavLink>
+      <NavLink
+      to="/directors"
+      exact
+      style={linkStyles}
+      activeStyle={{
+        background: 'pink'
+      }}
+      >
+        Directors
+      </NavLink>
+      <NavLink
+      to='/actors'
+      exact
+      style={linkStyles}
+      activeStyle={{
+        background: 'pink'
+      }}
+      >
+        Actors
+      </NavLink>
+
+    </div>
+  )
 }
 
 export default NavBar;
